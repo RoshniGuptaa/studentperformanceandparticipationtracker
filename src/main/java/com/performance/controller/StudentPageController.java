bdb4ee662d74752c7ae1b4486d46c83c7fe26365
RoshniGuptaa <128632897+RoshniGuptaa@users.noreply.github.com>
2025-07-03 11:42:41 +0530

    download result option added

--- src/main/java/com/performance/controller/StudentPageController.java
+++ src/main/java/com/performance/controller/StudentPageController.java
@@ -1,8 +1,9 @@
 package com.performance.controller;
 
 import java.security.Principal;
-import java.util.Optional;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.performance.dao.PerformanceRepository;
+import com.performance.dao.StudentRepository;
+import com.performance.dao.UserRepository;
+
 import com.performance.helper.RegisterParticipationRequest;
+
+
+
 
 @Controller
 @RequestMapping("/student")
 @PreAuthorize("hasRole('STUDENT')")
 public class StudentPageController {
 
+	@Autowired
+	private StudentRepository studentRepository;
+	@Autowired
+	private PerformanceRepository performanceRepository;
+	@Autowired
+	private UserRepository userRepository;
 	
 	@ModelAttribute
     public void addCommonData(Model model, Principal principal) {
@@ -37,5 +51,5 @@ public class StudentPageController {
 	}
 
 	
-
+	
 }
